46ff92c851714a243d95ef8a21d9c67eaee9c71a
dozman <chiedozieonyekwum.com>
2022-05-26 16:23:25 -0500

    terraform tips without the use of inline blocks

---
+++ inline_block.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "my_alb" {
+  name = "${var.service_name}-alb"
+  ingress {
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
+  }
+  egress {
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
+  }
+}
